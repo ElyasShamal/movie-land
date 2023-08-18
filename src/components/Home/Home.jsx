6ff55b327d6798ec3dda8250376992a3c82b7a10
Elyas <Elyas.shamal2@gmail.com>
2023-08-18 17:52:58 -0400

    add css for home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Form from "./Form";
 
 function Home() {
   return (
     <main className="container">
-      <section></section>
+      <section className="home-section">
+        <article className="article">
+          <p style={{ fontSize: "3rem" }}>
+            Unlimited movies, TV shows, and more
+          </p>
+          <p style={{ paddingTop: "1rem" }}>Watch anywhere. Cancel anytime.</p>
+        </article>
+        <Form />
+      </section>
     </main>
   );
 }
