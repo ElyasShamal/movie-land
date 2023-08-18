977f92736a54bf7f7ad77597d4ee36689e44b4a2
Elyas <Elyas.shamal2@gmail.com>
2023-08-17 23:42:20 -0400

    add code on movies .js

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+// api key = 2864abde
 
 function Movie() {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setMovies(data);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <section>
       <h1 style={{ textAlign: "center", color: "red" }}>movie page</h1>
