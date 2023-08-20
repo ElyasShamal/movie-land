1857227264574137078136d24ed0f60439840cb8
Elyas <Elyas.shamal2@gmail.com>
2023-08-19 22:27:48 -0400

    completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 function Footer() {
   return (
     <footer className="footer">
-      <h2>created by Elyas Shamal</h2>
+      <h3>created by Elyas Shamal</h3>
       <a href="https://github.com/ElyasShamal" target="-blink">
         <FaGithub />
       </a>
